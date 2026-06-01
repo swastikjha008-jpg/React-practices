5d726629b361a72161eb4cccdb3631b975a5f868
Swastik <swastikjha008@gmail.com>
2026-06-01 19:08:16 +0530

    Add TodoList component with TodoItem functionality

---
+++ TodoList.jsx
@@ -0,0 +1,28 @@
+import { useRecoilState } from "recoil";
+import { todoAtomFamily } from "../atom";
+
+function TodoItem({ id }) {
+  const [todo, setTodo] = useRecoilState(todoAtomFamily(id));
+  return (
+    <div>
+      <input
+        type="text"
+        value={todo.text}
+        onChange={(e) => setTodo({ ...todo, text: e.target.value })}
+        placeholder={`Todo ${id}`}
+      />
+      <button onClick={() => setTodo({ ...todo, done: !todo.done })}>
+        {todo.done ? "✅ Done" : "⬜ Mark done"}
+      </button>
+    </div>
+  );
+}
+
+export default function TodoList() {
+  return (
+    <div>
+      <h2>Todo List (atomFamily)</h2>
+      {[1, 2, 3, 4, 5].map((id) => <TodoItem key={id} id={id} />)}
+    </div>
+  );
+}
